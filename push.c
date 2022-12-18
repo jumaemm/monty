dae13dcbed210e4193944408ab0ac985cced1824
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-18 09:30:24 +0000

    Push node onto stack

---
+++ push.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * _push - pushes an item onto the stack
+ * @stack: stack given as arg
+ * @line_count: lines
+ * Return: Always 0
+ */
+void _push(stack_t **stack, unsigned int line_count)
+{
+	char *n = bus.arg;
+
+	if ((is_digit(n)) == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_count);
+		exit(EXIT_FAILURE);
+	}
+
+	if (bus.status == 1)
+	{
+		if (!add_node(stack, atoi(n)))
+		{
+			free_stack(*stack);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
