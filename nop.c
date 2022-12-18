4117e9c9be64a066db8dbdee8a0ff064e7ca362c
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-18 09:29:39 +0000

    does nothing

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - does nothing, nada
+ * @stack: pointer to stack
+ * @line_count: line counter
+ * Return: Always 0
+ */
+void _nop(stack_t **stack, unsigned int line_count)
+{
+	(void) line_count;
+	(void) stack;
+}
