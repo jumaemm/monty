e284540dfaea5b477d709f3f0c7e9962cf82a886
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-18 10:42:15 +0000

    Print string

---
+++ pstr.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * _pstr - prints the contents of a stack_t stack as a string
+ * @stack: pointer to stack
+ * @line_count: line counter for error messages
+ *
+ * Return: nothing
+ */
+void _pstr(stack_t **stack, unsigned int line_count)
+{
+	(void) line_count;
+	stack_t *current = *stack;
+
+	while (current)
+	{
+		if (current->n <= 0 || current->n > 127)
+			break;
+		putchar((char) current->n);
+		current = current->next;
+	}
+	putchar('\n');
+}
