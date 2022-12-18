16b5f2a709525bcfe74fac6c483f7c298e8e7ff3
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-18 10:44:01 +0000

    Num check

--- utils.c
+++ utils.c
@@ -19,3 +19,22 @@ int is_digit(char *string)
 	}
 	return (1);
 }
+/**
+ * isnumber - checks if a string is a number
+ * @string: provided string
+ *
+ * Return: 1 otherwise 0
+ */
+int isnumber(char *string)
+{
+	int i;
+
+	if (!string)
+		return (0);
+
+	for (i = 0; string[i]; i++)
+		if (i < '0' || i > '9')
+			return (0);
+
+	return (1);
+}
